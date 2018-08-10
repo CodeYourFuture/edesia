cfa7b9a04bcfab26a76d179b04c24eeeeda4c51c
Mat <lumierematerne11@gmail.com>
2018-08-10 20:41:55 +0100

    import Notifications into app.js

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,34 +1,71 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
 import Home from "../Home/Home";
 import Deliveries from "../Deliveries/Deliveries";
 import DriversRegistration from "../Drivers/Register";
+import {
+  link
+} from "fs";
+import Notifications from "../Notifications/Notifications"
 
 class App extends Component {
   render() {
-    return (
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/deliveries">Deliveries</Link>
-            </li>
-            <li>
-              <Link to="/drivers-register">Drivers Registration</Link>
-            </li>
-          </ul>
+    return ( <
+      Router >
+      <
+      div >
+      <
+      ul >
+      <
+      li >
+      <
+      Link to = "/" > Home < /Link> <
+      /li> <
+      li >
+      <
+      Link to = "/deliveries" > Deliveries < /Link> <
+      /li> <
+      li >
+      <
+      Link to = "/drivers-register" > Drivers Registration < /Link> <
+      /li> <
+      li >
+      <
+      Link to = "/notifications" > Notifications < /Link> <
+      /li>
 
-          <hr />
+      <
+      /ul>
 
-          <Route exact path="/" component={Home} />
-          <Route path="/deliveries" component={Deliveries} />
-          <Route path="/drivers-register" component={DriversRegistration} />
-        </div>
-      </Router>
+
+      <
+      hr / >
+
+      <
+      Route exact path = "/"
+      component = {
+        Home
+      }
+      /> <
+      Route path = "/deliveries"
+      component = {
+        Deliveries
+      }
+      /> <
+      Route path = "/drivers-register"
+      component = {
+        DriversRegistration
+      }
+      /> <
+      /div> <
+      /Router>
     );
   }
 }
