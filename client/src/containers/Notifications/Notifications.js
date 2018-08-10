f14482638cfc68e34da808da3ac33c64136c3eee
Mat <lumierematerne11@gmail.com>
2018-08-10 20:29:34 +0100

    add Notifications.js to client

---
+++ client/src/containers/Notifications/Notifications.js
@@ -0,0 +1,40 @@
+import React, {
+    Component
+} from "react";
+import * as api from "../../helpers/api";
+
+class Notifications extends Component {
+    state = {
+        notifications: []
+    };
+    componentDidMount() {
+        api.getNotifications().then(data => {
+            this.setState({
+                notifications: data
+            })
+        })
+    }
+
+    render() {
+        return <div >
+            <
+            h1 > List of Notifications < /h1> <
+            div > {
+                this.state.notifications.map(notification => {
+                    return ( < div >
+                        <
+                        p > {
+                            notifications.notificationsId
+                        } < /p> <
+                        p > {
+                            notifications.note
+                        } < /p> <
+                        /div>
+                    )
+                })
+            } <
+            /div> <
+            /div>
+    }
+
+}
