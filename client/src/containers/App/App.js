f8e99e8fe09a12b94a1b799d200281bddca1c47e
Mat <lumierematerne11@gmail.com>
2018-08-11 15:07:36 +0100

    add proper formatting

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,71 +1,35 @@
-import React, {
-  Component
-} from "react";
+import React, { Component } from "react";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "../Home/Home";
 import Deliveries from "../Deliveries/Deliveries";
 import DriversRegistration from "../Drivers/Register";
-import {
-  link
-} from "fs";
-import Notifications from "../Notifications/Notifications"
 
 class App extends Component {
   render() {
-    return ( <
-      Router >
-      <
-      div >
-      <
-      ul >
-      <
-      li >
-      <
-      Link to = "/" > Home < /Link> <
-      /li> <
-      li >
-      <
-      Link to = "/deliveries" > Deliveries < /Link> <
-      /li> <
-      li >
-      <
-      Link to = "/drivers-register" > Drivers Registration < /Link> <
-      /li> <
-      li >
-      <
-      Link to = "/notifications" > Notifications < /Link> <
-      /li>
-
-      <
-      /ul>
-
-
-      <
-      hr / >
-
-      <
-      Route exact path = "/"
-      component = {
-        Home
-      }
-      /> <
-      Route path = "/deliveries"
-      component = {
-        Deliveries
-      }
-      /> <
-      Route path = "/drivers-register"
-      component = {
-        DriversRegistration
-      }
-      /> <
-      /div> <
-      /Router>
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/"> Home </Link>
+            </li>
+            <li>
+              <Link to="/deliveries"> Deliveries </Link>
+            </li>
+            <li>
+              <Link to="/drivers-register"> Drivers Registration </Link>
+            </li>
+            <li>
+              <Link to="/notifications"> Notifications </Link>
+            </li>
+          </ul>
+          <hr />
+          <Route exact path="/" component={Home} />
+          <Route path="/deliveries" component={Deliveries} />
+          <Route path="/drivers-register" component={DriversRegistration} />
+        </div>
+      </Router>
     );
   }
 }
