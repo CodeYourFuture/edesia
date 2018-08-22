0b61160673a6c3d2b85fec5d9733c34d7c7fc190
Awet Fessha <awetkifle94@gmail.com>
2018-08-22 19:13:21 +0100

    Add axios and create loginUser and getUserProfile

--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -1,7 +1,22 @@
 const API_URL = process.env.REACT_APP_API_URI || "http://localhost:4000";
+const axios = require("axios");
+const instance = axios.create({
+  baseURL: API_URL
+});
 
 export const getUsers = () => {
   return fetch(`${API_URL}/api/users`).then(res => res.json());
 };
 
-export const getDelivers = () => {};
+export const getDelivers = () => {
+  return fetch(`${API_URL}/api/deliveries`).then(res => res.json());
+};
+export const getUserProfile = () => {
+  return instance.get("/user/profile");
+};
+export const loginUser = async (email, password) => {
+  const { data } = await instance.post("/auth/login", { email, password });
+  localStorage.setItem("jwtToken", data.token);
+  axios.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
+  return data.token;
+};
