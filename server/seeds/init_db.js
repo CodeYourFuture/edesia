28d6fa3639177c272f0220f94a55caa5484c9a92
Awet Fessha <awetkifle94@gmail.com>
2018-08-22 21:59:59 +0100

    update seed by adding city and postcode

--- server/seeds/init_db.js
+++ server/seeds/init_db.js
@@ -16,8 +16,20 @@ exports.seed = async (knex, Promise) => {
     .then(function() {
       // Inserts seed entries
       return knex("users").insert([
-        { name: "mat", email: "mat@gmail.com", password: "password1" },
-        { name: "awet", email: "awet@yahoo.com", password: "password2" }
+        {
+          name: "mat",
+          email: "mat@gmail.com",
+          password: "password1",
+          city: "Glasgow",
+          postcode: "G3 4EF"
+        },
+        {
+          name: "awet",
+          email: "awet@yahoo.com",
+          password: "password2",
+          city: "Glasgow",
+          postcode: "G1 5EF"
+        }
       ]);
     });
 
