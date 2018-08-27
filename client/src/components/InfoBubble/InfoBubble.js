4c343866603455c52a85928b2854a3cff97e5581
Awet Fessha <awetkifle94@gmail.com>
2018-08-27 18:59:04 +0100

    change the component from class to function and add prop types

--- client/src/components/InfoBubble/InfoBubble.js
+++ client/src/components/InfoBubble/InfoBubble.js
@@ -1,24 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import "./Info.css";
-class InfoBubble extends Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="info">
-          <h2>{this.props.number}</h2>
-          <h3>{this.props.text}</h3>
-        </div>
-        <div className="info">
-          <h2>40</h2>
-          <h3>Succesfull deliveries</h3>
-        </div>
-        <div className="info">
-          <h2>10</h2>
-          <h3>Missed deliveries</h3>
-        </div>
-      </div>
-    );
-  }
-}
 
+const InfoBubble = props => {
+  return (
+    <div className="container">
+      <div className="info">
+        <h2>{props.number}</h2>
+        <h3>{props.text}</h3>
+      </div>
+      <div className="info">
+        <h2>40</h2>
+        <h3>Succesfull deliveries</h3>
+      </div>
+      <div className="info">
+        <h2>10</h2>
+        <h3>Missed deliveries</h3>
+      </div>
+    </div>
+  );
+};
 export default InfoBubble;
+InfoBubble.PropTypes = {
+  number: PropTypes.number,
+  text: PropTypes.string
+};
