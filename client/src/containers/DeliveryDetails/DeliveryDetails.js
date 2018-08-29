22a338a397f9c9ceb3a1eb00b40f2ea8e0de596d
Awet Fessha <awetkifle94@gmail.com>
2018-08-30 00:08:53 +0100

    error message when match not found

--- client/src/containers/DeliveryDetails/DeliveryDetails.js
+++ client/src/containers/DeliveryDetails/DeliveryDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getDeliveryById } from "../../helpers/api";
+
 class DeliveryDetails extends Component {
   constructor() {
     super();
@@ -11,35 +12,40 @@ class DeliveryDetails extends Component {
   componentDidMount() {
     console.log(this.props);
     const delivery_id = this.props.match.params.deliveryId;
-    try {
-      console.log(delivery_id);
-      getDeliveryById(delivery_id).then(data => {
-        console.log(data);
-        this.setState({
-          delivery: data.data
-        });
-      });
-    } catch (err) {
+    console.log(delivery_id);
+    getDeliveryById(delivery_id).then(data => {
+      console.log(data);
       this.setState({
-        message: "No matching delivery was found in our system"
+        delivery: data.data
       });
-    }
+      console.log(this.state.delivery);
+    });
   }
   render() {
-    return (
-      <div>
-        <h4> Delivery Details</h4>
-        <p>
-          Delivery Id <strong>{this.state.delivery.delivery_id}</strong>
-        </p>
-        <p>
-          Delivery address <strong>{this.state.delivery.address}</strong>
-        </p>
-        <p>
-          Delivery deadline <strong> {this.state.delivery.deadline}</strong>
-        </p>
-      </div>
-    );
+    const deliveryInfo = this.state.delivery;
+    const message = "No matching delivery was found in our system";
+    if (deliveryInfo === {}) {
+      return (
+        <div>
+          <h4> Delivery Details</h4>
+          <p>
+            Delivery Id <strong>{deliveryInfo.delivery_id}</strong>
+          </p>
+          <p>
+            Delivery address <strong>{deliveryInfo.address}</strong>
+          </p>
+          <p>
+            Delivery deadline <strong> {deliveryInfo.deadline}</strong>
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h4> {message} </h4>
+        </div>
+      );
+    }
   }
 }
 
